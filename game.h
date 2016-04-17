bebacfd793b175517991b4d1e31ab9c56f7b5b47
sheree-enlow <kitsy3@hotmail.com>
2016-04-17 16:49:13 -0700

    Create game.h

---
+++ game.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SDL.h>     
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cctype>
+#include <string>
+#include <sstream>
+using namespace std;
+
+void fail(const char *msg);
